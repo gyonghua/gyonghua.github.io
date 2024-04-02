33c252889e6f12b2652284991ea87a16ceaec44a
Goh Yong Hua <gyonghua@users.noreply.github.com>
2024-04-02 10:27:46 +0800

    fix timing bug. Need to pass in us format

--- base.js
+++ base.js
@@ -5,22 +5,24 @@ const longOffsetString = longOffsetFormatter.format(new Date("2013-02-28T19:00:0
 // longOffsetString.split('GMT')[1] will give us '-05:00'
 const gmtOffset = longOffsetString.split('GMT')[1];
 
-let retirementDate = new Date("2024-12-31T12:00:00.000").toLocaleString("en-GB", {timeZone: "Asia/Singapore"});
+let retirementDate = new Date("2024-12-31T12:00:00");
+retirementDate = retirementDate.toLocaleString("en-US", {timeZone: "Asia/Singapore"})
+retirementDate = new Date(retirementDate)
+retirementDate = retirementDate.getTime()
 // console.log(retirementDate)
-retirementDate = new Date(retirementDate).getTime()
 
 // countdown
 let timer = setInterval(function() {
 
   // get today's date
-  let today = new Date();
-  today = today.toLocaleString("en-GB", {timeZone: "Asia/Singapore"})
+  let today = new Date().toLocaleString("en-US", {timeZone: "Asia/Singapore"});
 //   console.log(today)
   today = new Date(today).getTime()
   
   // get the difference
   let diff;
   diff = retirementDate - today;
+//   console.log(diff)
 
 
   // math
